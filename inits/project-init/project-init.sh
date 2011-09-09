5788b037e45485efab7e7c51e4e6ee8e822b86ad
Dan Bruegge <daniel@freshmilk.de>
2011-09-09 10:37:08 +0200

    Add configs/ and inits/

---
+++ inits/project-init/project-init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -x
+
+init
+{
+    #virtualenv
+    virtualenv $1 && cd $1
+    source bin/activate
+
+    #buildout
+    bin/easy_install zc.buildout
+    mkdir buildout && cd buildout
+    buildout init
+    wget http://svn.zope.org/*checkout*/zc.buildout/trunk/bootstrap/bootstrap.py
+    svn export svn+ssh://develop/utils/configs/buildout/buildout.cfg
+    python bootstrap.py
+}
